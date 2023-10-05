b60a6d7746293829957c5fbfa07c3e2695635b36
itzJenit <76831307+JenitNakrani@users.noreply.github.com>
2023-10-05 15:51:05 +0530

    Create problem_searching.py

---
+++ problem_searching.py
@@ -0,0 +1,37 @@
+# Find a position of first infinite number
+# Input: [20,-30,10,8,7,0,29,infinite,infinite,infinite,infinite]
+# Output: 7
+
+# Binary Search to find infinite number
+# Time Complexity: O(logn)
+def binary_search(array,i,j,target):
+    while  i <= j:
+        mid = i + (j-i) // 2
+        if array[mid] != target:
+            return binary_search(array,mid+1,j,target)
+        else:
+            if array[mid-1] < target:
+                return mid
+            else:
+                return binary_search(array,i,mid-1,target)
+
+# Linear Search to find infinite number
+# Time Complexity: O(n)
+def linearSearch(array, target):
+    for i in range(len(array)):
+        if array[i] == target:
+            return i
+    return -1
+
+# Inputs
+array = [20,-30,10,8,7,0,29,float('inf'),float('inf')float('inf'),float('inf')]
+start = 0
+end = 10
+target = float('inf')
+
+# Results
+result_linear_search = linearSearch(array, target)
+result_binary_search = binary_search(array, start, end, target)
+
+print("First infinite number is at index(Linear Search):",result_linear_search)
+print("First infinite number is at index(Binary Search):",result_binary_search)
